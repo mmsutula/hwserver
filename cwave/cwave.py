0d9e43faa7cd2d069cf96a9b945ac1b891419dd4
mmsutula <mmsutula@mit.edu>
2021-11-05 14:34:05 -0400

    initial tuning

--- cwave/cwave.py
+++ cwave/cwave.py
@@ -1,28 +1,29 @@
-import socket, select
-import logging, re, time, os, subprocess
+import sys, time
 import ctypes
+import logger 
 
 logger = logging.getLogger(__name__)
 
+ def init(name, cwave_addr):
+
+    try: 
+        ctypes.windll.kernel32.SetDllDirectoryW('C:/Program Files/CWave/x64')
+        self.lib = ctypes.cdll.LoadLibrary('CWAVE_DLL.dll')
+        logger.debug('Loaded DLL')
+    except: 
+        logger.error('Failed to load DLL')
+
+    global ADDR 
+    ADDR = 'cwave', cwave_addr
+
+    self.laser = cwave.cwave_connect(ADDR.encode('ASCII'))
+    if ret == 1
+        pass 
+    else 
+        logger.error('Failed to connect to CWave')
+
 
 class cwave:
-    # DLLpath -> path to DLL file
-    # HeaderPath -> path to header file 
-
-    DLLpath = r'C:/path.dll'
-    HeaderPath = r'C:/path.h'
-
-    def __init__(self, cwave_addr):
-        self.lib = windll.LoadLibrary(cwave.DLLpath)
-        with open(cwave.HeaderPath,'rt') as f:
-            self.header = f.read().split('\n')
-        try:
-            self.dispatch()
-        except: 
-            logger.exception('Failed to launch cwave')
-        global ADDR 
-        ADDR = 'cwave', cwave_addr
-        self.laser = cwave()
 
     def __enter__(self):
         return self
@@ -30,6 +31,7 @@ class cwave:
     def __exit__(self, exc_type, exc_value, traceback)
         self._Close()
 
-    def dispatch(self,client,fn,timeout=60):
+    def dispatch(self,client,wl):
         tstart = time.time()
-        out = cwave.cwave_connect(ipAddr.encode('ASCII'))
+        #todo: add wavelength range for vis/IR to tune OPO or SHG 
+        coarsetune = self.set_intvalue(b'opo_lambda',wavelength*100)
